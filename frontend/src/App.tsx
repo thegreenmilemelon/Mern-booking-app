1b9afa394c3d44e9bdaa5bf9a70f1735fbdb47f1
greenmelon <suren78621@gmail.com>
2024-05-25 23:45:36 +0545

    initial setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
+
